d8b2078bac6ef32b11c3a88ac0dc1134854050c1
rinatsakaev <r.sakaevv@gmail.com>
2018-12-21 23:28:57 +0500

    fix

--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
+from flask import Flask
+from database import db
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
+db.init_app(app)
 
-db = SQLAlchemy(app)
+from Models.User import User
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
